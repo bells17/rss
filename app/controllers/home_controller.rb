f548c9e975e3371d309853816068da1bff7871cc
bells17 <bells171@gmail.com>
2016-02-25 01:54:02 +0900

    Add feed contents

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,11 @@ class HomeController < BaseController
     urls = ['https://www.nxsw.co.jp/feed/']    
     feeds = urls.map do |url|
       Feedjira::Feed.fetch_and_parse url
+      # doc = Nokogiri::HTML.parse(html, nil, charset)
+      # parseしてはじめに見つかったimgを設定 or なかったら noimage を設定
     end
     @data = { :feeds => feeds }
     p @data
+    p @data[:feeds][0].entries[0].keys
   end
 end
